0bb74ac3e1b9a084192d11669c7268a21cd9a8cc
torresposso <erictorres.co@gmail.com>
2026-02-12 01:03:54 -0500

    Apply Design C: Impacto Patriótico - Consistent Header, New Article Cards & Layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Lucio Torres';
-export const SITE_DESCRIPTION = 'Análisis político y periodístico';
+export const SITE_TITLE = 'Lucio Torres - Precandidato Presidencial Colombia 2026';
+export const SITE_DESCRIPTION = 'Lucio Torres candidato independiente en la consulta "Frente Por La Vida". Propuestas: seguridad ciudadana, educación gratis, salud digna, empleo digno, soberanía nacional. Vota el 8 de marzo.';
