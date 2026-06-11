18129e9a707906dffbcc91fe38bcf174a717e33b
torresposso <erictorres.co@gmail.com>
2026-06-11 09:53:27 -0500

    fix: replace undefined variable siteName with standard get_bloginfo

--- web/app/themes/luciotorres/resources/views/sections/footer.blade.php
+++ web/app/themes/luciotorres/resources/views/sections/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="content-info bg-neutral text-neutral-content py-8 px-6 mt-12 text-center">
-  <p>&copy; {{ date('Y') }} {{ $siteName }}. {{ __('Todos los derechos reservados.', 'luciotorres') }}</p>
+  <p>&copy; {{ date('Y') }} {{ get_bloginfo('name') }}. {{ __('Todos los derechos reservados.', 'luciotorres') }}</p>
   @php(dynamic_sidebar('sidebar-footer'))
 </footer>
--- web/app/themes/luciotorres/resources/views/sections/header.blade.php
+++ web/app/themes/luciotorres/resources/views/sections/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner bg-base-200 py-4 px-6 flex justify-between items-center">
   <a class="brand font-bold text-xl" href="{{ home_url('/') }}">
-    {{ $siteName }}
+    {{ get_bloginfo('name') }}
   </a>
 
   @if (has_nav_menu('primary_navigation'))
